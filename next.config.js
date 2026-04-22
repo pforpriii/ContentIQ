ae0ffaf8d51f21f7bc4f972b742df3f5cb973ef8
Priyanshi Goyal <2020pietadpriyanshi39@poornima.org>
2026-04-22 21:49:59 +0530

    fix webpack alias

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
+  },
+}
+module.exports = nextConfig
